1c8fa58b79faac5d4bfa37b858180c2abc243fc9
noblecook <patrick.d.cook@gmail.com>
2015-01-24 19:35:44 -0600

    2015-01-24 HelloWorld Project:  First github project

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author patri_000
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+
+	}
+
+}
