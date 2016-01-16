d1b9ed99674917d966a498aeb94a1e6c933d7db3
noblecook <patrick.d.cook@gmail.com>
2016-01-16 16:34:03 -0600

    downloaded and made a change to upload

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -10,10 +10,14 @@ public class HelloWorld {
 
 	/**
 	 * @param args
+	 * File->import->Git->projets from Git->
+	 * Then choose option for eclipse
 	 */
+	
 	public static void main(String[] args) {
 		
 		System.out.println("Hello World!");
+		System.out.println("Hello Patrick - changed!");
 
 	}
 
